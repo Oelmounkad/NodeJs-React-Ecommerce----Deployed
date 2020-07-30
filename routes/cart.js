257259037343cc87f9fbd661dcf1146458d6d801
Elmounkad <oelmounkad@gmail.com>
2020-07-30 22:39:52 +0200

    adding to cart works fine

--- routes/cart.js
+++ routes/cart.js
@@ -10,16 +10,33 @@ const CartItem = require('../models/CartItem')
 // @desc Get all the items in the user's cart
 // @access Private
 
-router.get('/', async (req,res) => {
+router.get('/', auth , async (req,res) => {
     try {
-         const cartitems = await CartItem.find()
-         .populate([{path:'user',select : 'name'},{path:'product',select : 'name description price'}])
-
+         const cartitems = await CartItem.find({user: req.user.id})
+         .populate([{path: 'product',select: 'name price'}])
+        
          res.json(cartitems)
     } catch (err) {
          res.status(500).send('Server Error')
     } 
 })
+
+
+
+// @route GET /api/cart
+// @desc Get all the items in the user's cart
+// @access Private
+
+router.get('/:id', async (req,res) => {
+     try {
+          const cartitem = await CartItem.findById(req.params.id)
+         
+          res.json(cartitem)
+     } catch (err) {
+          res.status(500).send('Server Error')
+     } 
+ })
+
 
 // @route   POST api/cart
 // @desc    Add a cart item for a user
