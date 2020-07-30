0ece42bca24b5e623410ab1701bad76906576c13
Elmounkad <oelmounkad@gmail.com>
2020-07-30 20:14:04 +0200

    cart refactored

--- client/src/components/cart/CartItem.js
+++ client/src/components/cart/CartItem.js
@@ -3,36 +3,37 @@ import React from 'react'
  const CartItem = ({item}) => {
     return (
         <>
-        <div className="row">
-                        <div className="col-12 col-sm-12 col-md-2 text-center">
-                                <img className="img-responsive" src="http://placehold.it/120x80" alt="prewiew" width="120" height="80" />
-                        </div>
-                        <div className="col-12 text-sm-center col-sm-12 text-md-left col-md-6">
-                            <h4 className="product-name"><strong>{item.product.name}</strong></h4>
-                            <h4>
-                            <small> { item.product.description } </small>
-                            </h4>
-                        </div>
-                        <div className="col-12 col-sm-12 text-sm-center col-md-4 text-md-right row">
-                            <div className="col-3 col-sm-3 col-md-6 text-md-right" style={{paddingTop: "5px"}}>
-                                <h6><strong>{item.product.price} <span className="text-muted">x</span></strong></h6>
-                            </div>
-                            <div className="col-4 col-sm-4 col-md-4">
-                                <div className="quantity">
-                                    <input type="button" value="+" className="plus" />
-                                    <input type="number" step="1" max="99" min="1" value="1" title="Qty" className="qty"
-                                           size="4" name='quantity' value={item.quantity} />
-                                    <input type="button" value="-" className="minus" />
-                                </div>
-                            </div>
-                            <div className="col-2 col-sm-2 col-md-2 text-right">
-                                <button type="button" className="btn btn-outline-danger btn-xs">
-                                    <i className="fa fa-trash" aria-hidden="true"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <hr/>
+       
+                             <li class="cart_item clearfix">
+                                 <div class="cart_item_image"><img src="" alt="" /></div>
+                                 <div class="cart_item_info d-flex flex-md-row flex-column justify-content-between">
+                                     <div class="cart_item_name cart_info_col">
+                                         <div class="cart_item_title">Name</div>
+                                    <div class="cart_item_text">{item.product.name}</div>
+                                     </div> 
+                                     <div class="cart_item_quantity cart_info_col">
+                                         <div class="cart_item_title">Quantity</div>
+                                         <div class="cart_item_text">{item.quantity}</div>
+                                     </div>
+                                     <div class="cart_item_price cart_info_col">
+                                         <div class="cart_item_title">Price</div>
+                                         <div class="cart_item_text">{item.product.price}</div>
+                                     </div>
+                                     <div class="cart_item_total cart_info_col">
+                                         <div class="cart_item_title">Total</div>
+                                    <div class="cart_item_text">{ item.product.price*item.quantity  }</div>
+                                     </div>
+                                     <div class="cart_item_color cart_info_col">
+                                         <div class="cart_item_title">Action</div>
+                                         <div class="cart_item_text">
+                                              <button className="btn btn-danger"><i class="fas fa-trash" /></button>
+                                              <button className="btn btn-success"><i class="far fa-check-circle"/></button>
+                                               </div>
+                                     </div>
+                                 </div>
+                             </li>
+                             <hr/>
+                        
                     </>
     )
 }
--- client/src/components/pages/MyCart.js
+++ client/src/components/pages/MyCart.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useContext} from 'react'
+import React,{useEffect,useContext,useState} from 'react'
 import CartContext from '../../context/cart/CartContext'
 import CartItem from '../cart/CartItem'
 import {Spinner} from 'react-bootstrap'
@@ -9,25 +9,38 @@ import {Spinner} from 'react-bootstrap'
  const cartContext =  useContext(CartContext)
  const {cart,getCartItems} = cartContext
 
+ const [state, setstate] = useState('');
+   
+ const calculateTotal = c => {
+
+      let total = 0
+      c.forEach(x => total = total + x.quantity * x.product.price)
+      console.log('total : '+total)
+      return total
+ }
+
+
  useEffect(() => {
 
    getCartItems()
-    
  }, []);
 
 
     return (
        <>
-      <div className="container">
-    <div className="card shopping-cart">
-            <div className="card-header bg-dark text-light">
-                <i className="fa fa-shopping-cart" aria-hidden="true"></i>{' '}
-                Shopping cart
+        <div class="cart_section">
+     <div class="container-fluid">
+         <div class="row">
+             <div class="col-lg-10 offset-lg-1">
+                 <div class="cart_container">
+    <div class="cart_title">Shopping Cart<small> ({cart !== null && <> {cart.length} </>} 
+    item{ cart !== null && cart.length == 1 ? <></> : <>s</>  } in your cart) </small></div>
+                     <div class="cart_items">
+                         <ul class="cart_list">
+           
+              {cart !== null ? cart.map(item => <CartItem key={item._id} item={item} /> )
               
-                <div className="clearfix"></div>
-            </div>
-            <div className="card-body">
-              {cart !== null ? cart.map(item => <CartItem key={item._id} item={item} /> ) :
+              :
 
                    <div style={{
                      position: 'absolute', left: '50%', top: '50%',
@@ -38,23 +51,16 @@ import {Spinner} from 'react-bootstrap'
 
               }
                    
-                    <hr /> 
+                   </ul>
+                     </div>
                     
-                 
-            </div>
-            <div className="card-footer">
-                <div className="coupon col-md-5 col-sm-5 no-padding-left pull-left">
-                  
-                </div>
-                <div className="pull-right" style={{margin: "10px"}}>
-                    <a href="" className="btn btn-success pull-right">Checkout</a>
-                    <div className="pull-right" style={{margin: "5px"}}>
-                        Total price: <b>50.00€</b>
-                    </div>
-                </div>
-            </div>
-        </div>
-</div>
+                     <div class="cart_buttons"> <button type="button" class="button cart_button_clear">Continue Shopping</button></div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>       
+          
 </>
     )
 }
