1f57133891bb3e5f8b18a03425893f71374cbc4c
Elmounkad <oelmounkad@gmail.com>
2020-07-31 23:48:30 +0200

    prepare for deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const app = express()
-//const path = require('path')
+const path = require('path')
 
 //Connect DB
 connectDB()
@@ -24,5 +24,16 @@ app.use('/api/products',require('./routes/products'))
 
 // cart
 app.use('/api/cart',require('./routes/cart'))
+
+
+//Serve static assets in production
+
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')))
+}
+
 
 app.listen(PORT, () => console.log(`Server started on port : ${PORT}`))
