04e84d4c0a9e95594feca13defa01915d6d57c65
Elmounkad <oelmounkad@gmail.com>
2020-08-01 02:06:37 +0200

    added photo

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,17 +1,14 @@
 import React,{useContext,useEffect} from 'react'
-import AuthContext from '../../context/auth/AuthContext'
+import reactlogo from '../../rr.png'
 const Home = () => {
 
-    const authContext = useContext(AuthContext)
 
     return (
-        <div className="grid-2">
-           <div>
-               
-           </div>
-           <div>
-               
-           </div>
+        <div>
+            <img src={reactlogo} height='500px' alt=""/>
+           <h2 className="text-center">Welcome to the E-commerce Application</h2> 
+           <h5 className="text-center">Built With MERN : MongoDB / Express / React / NodeJs</h5>
+           <h5 className="text-center">Created by : Oussama Elmounkad</h5>
         </div>
     )
 }
